4e6317850181f9a3373e99c74a286b954aa162e8
jaanisar <jaanisark@gmail.com>
2020-09-21 18:37:39 -0700

    capturing changes previously saved in different files

--- tomatomation/watersense.py
+++ tomatomation/watersense.py
@@ -27,9 +27,9 @@ i=0
 # Scheduler Time Check
 current_time=time.localtime()
 if current_time.tm_hour == 8:
-	watering_time = True
+    watering_time = True
 else:
-	watering_time = False
+    watering_time = False
 
 # Manually Enabling
 watering_time = True
@@ -38,32 +38,33 @@ start_time = time.time()
 print("Sensing Started on "+str(current_time.tm_mon) + "/" + str(current_time.tm_mday) + "at "  + str(current_time.tm_hour) + ":" + str(current_time.tm_min) + "!")
 try:
 
-	while watering_time:
-		watering_complete = False
-		#start_time = time.time()
-		
-		if time.time() - start_time < 30:
-			pwm_pump.ChangeDutyCycle(100)
-		else:
-			i = GPIO.input(h2oPin)
-			if i == 1:
-				print("Water Detected! Pump Off")
-				pwm_led.ChangeDutyCycle(100)
-				pwm_pump.ChangeDutyCycle(0)
-				end_time=time.time()
-			elif time.time() - start_time > 60:
-				print("Pump Time Maxed Out, Shutting Off Pump!")
-				pwm_led.ChangeDutyCycle(0)
-				pwm_pump.ChangeDutyCycle(0)
-				end_time=time.time()
-			else:
-				print("No Water Detected. Continue Pumping")
-				pwm_led.ChangeDutyCycle(0)
-				pwm_pump.ChangeDutyCycle(100)
-		time.sleep(1)
-		print("Time Elapsed: ", time.time()-start_time)
-		
+    while watering_time:
+        watering_complete = False
+        #start_time = time.time()
+        
+        if time.time() - start_time < 30:
+            pwm_pump.ChangeDutyCycle(100)
+        else:
+            i = GPIO.input(h2oPin)
+            if i == 1:
+                print("Water Detected! Pump Off")
+                pwm_led.ChangeDutyCycle(100)
+                pwm_pump.ChangeDutyCycle(0)
+                end_time=time.time()
+            elif time.time() - start_time > 60:
+                print("Pump Time Maxed Out, Shutting Off Pump!")
+                pwm_led.ChangeDutyCycle(0)
+                pwm_pump.ChangeDutyCycle(0)
+                end_time=time.time()
+            else:
+                print("No Water Detected. Continue Pumping")
+                pwm_led.ChangeDutyCycle(0)
+                pwm_pump.ChangeDutyCycle(100)
+        time.sleep(1)
+        print("Time Elapsed: ", time.time()-start_time)
+        
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-	print("Program Interrupted By Keyboard")
-	GPIO.cleanup()
-	sys.exit()
+    print("Program Interrupted By Keyboard")
+    GPIO.cleanup()
+    sys.exit()
+

d66417f31fd27cc5d430d74352161fb7b120a5a5
jaanisar <jaanisark@gmail.com>
2020-12-13 18:14:34 -0800

    formatting cleanup

--- tomatomation/watersense.py
+++ tomatomation/watersense.py
@@ -4,9 +4,9 @@ import time
 import sys
 
 # Pin Definitions:
-h2oPin = 14 #BCM Pin 14, Board Pin 8
-pumpPin = 21 #BCM Pin 21, Board Pin 40
-ledPin = 15 #BCM
+h2oPin = 14 # BCM Pin 14, Board Pin 8
+pumpPin = 21 # BCM Pin 21, Board Pin 40
+ledPin = 15 # BCM
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM)
@@ -21,22 +21,26 @@ ledPWM.start(0)
 pumpPWM.start(0)
 
 # Sensing
-water_sense = 0 # Initial Water Sensor Value: False
-w_time_hr = 8 # Set Scheduler to Start at 8 AM
-watering_complete = False #initial value, may not be req'd
+water_sense = 0  # Initial Water Sensor Value: False
+w_time_hr = 8  # Set Scheduler to Start at 8 AM
+watering_complete = False  # initial value, may not be req'd
 running = True
 
 # Pump Actions
+
+
 def start_pump():
     pumpPWM.ChangeDutyCycle(100)
     print(str(time.ctime())+ ": " + "Starting Water Pump!")  
+
 
 def stop_pump():
     pumpPWM.ChangeDutyCycle(0) # Shutoff Pump
     ledPWM.ChangeDutyCycle(0) # Turn On Status LED   
     end_time = time.time() # Capture End Time
     total_duration = end_time - start_time
-    print(str(time.ctime())+ ": " +"Total Pumping Time = " + str(round(total_duration)) + " seconds")
+    print(str(time.ctime())+ ": " + "Total Pumping Time = " + str(round(total_duration)) + " seconds")
+
 
 # LED Flashing
 def single_flash_led(sleep_time):
@@ -48,22 +52,24 @@ def single_flash_led(sleep_time):
     ledPWM.ChangeDutyCycle(duty_low)
     time.sleep(sleep_time)
 
+
 def multi_flash_led(n_flashes, sleep_time):
-    for _ in range (n_flashes):
+    for _ in range(n_flashes):
         single_flash_led(sleep_time)
+
 
 try:
     while running:
-        c_time=time.localtime() #Get Current Time
-        w_time_hr = c_time.tm_hour # FORCE START FOR TESTING
+        c_time=time.localtime()  # Get Current Time
+        w_time_hr = c_time.tm_hour  # FORCE START FOR TESTING
         if c_time.tm_hour == w_time_hr:
-            print(str(time.ctime())+ ": " +"Watering Started!")
+            print(str(time.ctime()) + ": " + "Watering Started!")
             start_time = time.time()  
             start_pump()
             multi_flash_led(2, 0.1)
-            while watering_complete == False:
+            while not watering_complete:
                 water_sense = GPIO.input(h2oPin) # Check Water Sensor Status
-                if (time.time() - start_time) > 3 and water_sense == 1: # Minimum Pump Runtime: 3 seconds and Sensor Check
+                if (time.time() - start_time) > 3 and water_sense == 1:  # Minimum Pump Runtime: 3 seconds and Sensor Check
                     stop_pump()
                     watering_complete = True
                     print(str(time.ctime())+ ": " + "Water Detected! Pump Off")
@@ -84,7 +90,7 @@ try:
             multi_flash_led(3, 1)
             print("It's not time to water yet. Watering scheduled for hour #" + w_time_hr + " of 24")
 
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
     print("Program Interrupted By Keyboard")
     stop_pump()
     running = False
